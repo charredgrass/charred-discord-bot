2b85c57a771ecd5fc6dbc440c5cdec3a1f1a64b0
Maxwell Chow <maxchowla@gmail.com>
2017-10-18 09:15:27 -0700

    new leaderboard

--- bot.js
+++ bot.js
@@ -247,6 +247,9 @@ function game(args, user, send, mens, name, autoInit) {
   if (args[0] === "leaderboard") {
     send(ghandler.getLeaderboard(5));
   }
+  if (args[0] === "explb") {
+    send(ghandler.getExpLeaderboard(10));
+  }
   if (args[0] === "admin" && user === "154826263628873728") {
     if (args[1] === "set") {
       let person = args[2];
