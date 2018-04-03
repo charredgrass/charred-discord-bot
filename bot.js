78cb367b16920c5259c64d48168c3205770dbbbf
Maxwell Chow <maxchowla@gmail.com>
2018-04-02 18:29:43 -0700

    puppeteer commands to AtG

--- bot.js
+++ bot.js
@@ -423,6 +423,18 @@ client.on("message", message => {
     var answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
     send(getRandomFromList(answers));
   }
+  if (msg.substring(0, "!anchorloc".length).toLowerCase() === "!anchorloc") {
+    anchorloc = loc;
+    message.delete();
+  }
+  if (msg.substring(0, "!proxy".length).toLowerCase() === "!proxy") {
+    if (message.author.id == "154826263628873728") {
+      let toSay = msg.substring("!proxy ".length);
+      if (anchorloc) {
+        anchorloc.send(toSay);
+      }
+    }
+  }
 
   //RAO Commands
   if (shouldResp.rao === true) {
@@ -485,18 +497,6 @@ client.on("message", message => {
     }
     if (msg === "!jokes") {
       send("https://github.com/charredgrass/raocsgo-discord-bot/blob/master/texts/jokes.txt");
-    }
-    if (msg.substring(0, "!anchorloc".length).toLowerCase() === "!anchorloc") {
-      anchorloc = loc;
-      message.delete();
-    }
-    if (msg.substring(0, "!proxy".length).toLowerCase() === "!proxy") {
-      if (message.author.id == "154826263628873728") {
-        let toSay = msg.substring("!proxy ".length);
-        if (anchorloc) {
-          anchorloc.send(toSay);
-        }
-      }
     }
     if (hascmd(msg, "priceof")) {
       getPrice(msg.substring("!priceof ".length), 730, send);
