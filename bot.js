b893d10a98d4279fb173b724a5946a1ebb3fc2b3
Maxwell Chow <maxchowla@gmail.com>
2018-04-02 18:38:08 -0700

    new command

--- bot.js
+++ bot.js
@@ -418,6 +418,9 @@ client.on("message", message => {
   if (msg.toLowerCase() === "!source" || msg.toLowerCase() === "!sauce") {
     send("https://github.com/charredgrass/raocsgo-discord-bot");
   }
+  if (msg == "!amigay") {
+    send("yea");
+  }
 
   if (hascmd(msg, "magic8")) {
     var answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
@@ -620,7 +623,6 @@ client.on("message", message => {
     if (hascmd(msg, "tobees")) {
       let args = argify(msg, "tobees");
       let val = Number(args[0]);
-      console.log(val);
       if (isNaN(val) === false) {
         send("" + val + " USD is " + (20 * val) + " bees.");
       } else {
