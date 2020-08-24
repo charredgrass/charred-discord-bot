66b17ca886b80feeb8b39f2592ef16850d3d57ff
Maxwell Chow <maxchowla@gmail.com>
2020-08-24 04:06:10 -0700

    moving to desktop pc

--- newbot.js
+++ newbot.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const logger = require("./lib/logger.js");
 const utils = require("./lib/utils.js");
 
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 let config = JSON.parse(fs.readFileSync("./config.json").toString("utf-8"));
 
@@ -38,13 +38,53 @@ process.stdin.on("data", (text) => {
   }
 });
 
+function serverSelector(serverID) {
+  let ret = {
+    atg: false,
+    frz: false,
+    rao: false,
+    dnd: false
+  };
+  if (serverID === "167586953061990400") { //RAOCSGO
+    ret.rao = true;
+  } else if (serverID === "276220128822165505") { //AtG
+    ret.atg = true;
+  } else if (serverID === "234382619767341056") { //r/Frozen
+    ret.frz = true;
+  } else if (serverID === "446813545049358336") { //D&D
+    ret.dnd = true;
+  }else if (serverID === "220039870410784768") { //Clowns
+    ret.dnd = true;
+  } else if (serverID === "313169519545679872" || !serverID) { //nass and dmchannel
+    ret.atg = true;
+    ret.frz = true;
+    ret.rao = true;
+    ret.dnd = true;
+  }
+  return ret;
+}
+
 let modules = [{
   handle: (data) => {
-    if (data.message == "!ping") {
-      data.send("pong");
+    data.send("pong");
+  },
+  check: (args, selector, channelName) => {
+    if (args[0] == "!ping") {
+      return true;
+    }
+  }
+}, {
+  handle: (data) => {
+
+  }, 
+  check: (args, selector, channelName) => {
+    if (args[0] == "!stamrow") {
+      return true;
     }
   }
 }];
+
+modules.push(require("./lib/rolemanager.js"));
 
 //Main event listener for messages
 client.on("message", (message) => {
@@ -54,11 +94,13 @@ client.on("message", (message) => {
   let loc = message.channel;
   let msg = message.content;
 
-  let server;
+  let server, channelName;
   if (loc.guild) {
     server = loc.guild.id;
+    channelName = loc.name;
   }
-
+  selector = serverSelector(server);
+  let args = utils.argsplit(msg); //will be null if msg is not a !command, otherwise will be an array
   let mdata = {
     send: function(text, opts) {
       loc.send(text, opts).catch((err) => {
@@ -74,17 +116,25 @@ client.on("message", (message) => {
         send(text);
       }
     },
-    message: msg,
-    server: server
+    message: msg, //the message content
+    server: server, //the server id
+    selector: selector, //server selector object
+    channel: channelName, //channel name as string
+    args: args, //array of args
+    location: loc, //Channel object
+    author: message.author, //author object
+    member: message.member, //GuildMember object
+    delete: () => {
+      message.delete();
+    }, //delete function
+    client: client
   }
-  let a = utils.argsplit(msg); //will be null if msg is not a !command, otherwise will be an array
-  //TODO add filtering to only grab messages that fit command-form, to reduce on processing time
-  if (a) {
+  if (args) { //only if it is a valid !command
     for (let m of modules) {
       if (!(m.handle && typeof m.handle === 'function')) {
         logger.warn("A handler was configured incorrectly.");
-      } else if (m.checker && typeof m.checker === 'function') {
-        if (m.checker(a) === true) {
+      } else if (m.check && typeof m.check === 'function') {
+        if (m.check(args, selector, channelName) === true) {
           m.handle(mdata);
         }
       } else {
